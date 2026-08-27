c2e10f317630fdef3430b6d6682eec9c85b9455e
Samuel Nelo <snelo1390@gmail.com>
2026-08-27 09:39:02 -0400

    Primer commit

--- main.c
+++ main.c
@@ -23,6 +23,7 @@ ObjectData create_iron_sword() {
             int free_idx = get_idx_from_id(player, iron_sword.data.weapon.base_item.id);
             remove_item(player, free_idx);
             player->base_char.weapon = &iron_sword.data.weapon;
+            // Reconstruimos el arma previa
             ObjectData prev_w;
             prev_w.type = TYPE_WEAPON;
             prev_w.data.weapon = *previous_weapon;
@@ -37,11 +38,6 @@ ObjectData create_iron_sword() {
 }
 
 
-void equip_armor(Player* player, Armor* armor) {
-    player->base_char.defense = armor;
-    printf("%s se ha equipado con %s\n", player->base_char.name, armor->base_item.name);
-    enter_to_continue();
-}
 ObjectData  create_healh_potion(int health_percentage, char* subffix, int quantity) {
     int quant = quantity;
     if (quant == NULL || quant <= 0) {
@@ -73,24 +69,25 @@ ObjectData  create_healh_potion(int health_percentage, char* subffix, int quanti
 int main(void) {
     
     Weapon base_iron_sword = {TYPE_WEAPON,{"Espada de hierro", "Arma cuerpo a cuerpo muy popular y eficaz", 1, true, equip_sword}, 3, 100};
-    Armor base_iron_armor = {TYPE_ARMOR, {"Armadura de hierro", "Armadura que protege el cuerpo de ataques fisicos", 1, true, equip_armor}, 3, 100};
+    // Armor base_iron_armor = {TYPE_ARMOR, {"Armadura de hierro", "Armadura que protege el cuerpo de ataques fisicos", 1, true, equip_armor}, 3, 100};
 
-    ObjectData iron_sword, iron_armor;
+    ObjectData iron_sword;
     iron_sword.type = TYPE_WEAPON;
     iron_sword.data.weapon = base_iron_sword;
-    iron_armor.type = TYPE_ARMOR;
-    iron_armor.data.armor = base_iron_armor;
+    // iron_armor.type = TYPE_ARMOR;
+    // iron_armor.data.armor = base_iron_armor;
     ObjectData inventory_player[MAX_INVENTORY];
     Player player;
-    init_character(&player.base_char, "Caballero", 80, 100, 100, &iron_armor, 1, 100, 0, &iron_sword);
+    init_character(&player.base_char, "Caballero", 80, 100, 100, NULL, 1, 100, 0, &iron_sword);
     ObjectData health_potion_I = create_healh_potion(40, "I");
     ObjectData health_potion_II = create_healh_potion(70, "II");
+    
     player.inventory[0] = health_potion_I;
     player.inventory[1] = health_potion_II;
     player.inventory_count = 2;
     Character enemy1, enemy2;
-    init_character(&enemy1, "Goblin", 10, 80, 80, &iron_armor, 1, 100, 0, NULL);
-    init_character(&enemy2, "Goblin", 20, 90, 90, &iron_armor, 2, 110, 0, NULL);
+    init_character(&enemy1, "Goblin", 10, 80, 80, NULL, 1, 100, 0, NULL);
+    init_character(&enemy2, "Goblin", 20, 90, 90, NULL, 2, 110, 0, NULL);
     combat(&player, &enemy1);
     combat(&player, &enemy2);
     return 0;
